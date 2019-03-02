3aaa996e5f35906ecd38dd693e843d46079cd574
Andrew Santoso <santoso@berkeley.edu>
2019-03-02 11:35:02 -0800

    Initial Commit

---
+++ Lufthansa_Flight_Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lufthansa_Flight_Tracker
+//
+//  Created by Andrew Santoso on 3/2/19.
+//  Copyright © 2019 Andrew Santoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
